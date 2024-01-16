3f75f51c5e5a60337684705c00070faba5488709
techietimmy-ux <ikpediatimmy@gmail.com>
2024-01-16 23:31:18 +0100

    'naijaQuiz'

---
+++ quiz.js
@@ -0,0 +1,42 @@
+// array of  correct answers, form and display result
+
+const correctAnswers = ['B', 'A', 'B', 'B', 'B', 'B', 'A', 'A', 'A', 'A']
+const form = document.querySelector('form')
+const displayResult = document.querySelector('.result')
+const span = document.querySelector('span');
+
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    scrollTo(0, 0)
+    let score = 0;
+
+    // array of selected answers
+    const userAnswers = [form.Q1.value, form.Q2.value, form.Q3.value, form.Q4.value, form.Q5.value, form.Q6.value, form.Q7.value, form.Q8.value, form.Q9.value, form.Q10.value,]
+
+    // iterating over the userAnswers 
+
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            score += 10;
+        }
+    })
+    console.log(score);
+    displayResult.classList.remove('d-none')
+    // span.textContent=score
+
+    let animatedScore = 0;
+
+    const timer = setInterval(() => {
+        span.textContent =`${animatedScore}%`
+        if (animatedScore === score) {
+            clearInterval(timer)
+        } else {
+
+            animatedScore += 1
+
+        }
+    }, 10);
+
+})
